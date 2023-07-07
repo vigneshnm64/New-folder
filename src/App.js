4d2b23989ee795894e053cd485c2e1a1fa2b9f3c
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-07 09:00:04 +0530

    todays build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
       <p>All is good</p>
       <p>For build and deploy</p>
       <p>For build trigger </p>
-      <p>For build tigger concept with aws and jenkins </p>
+      <p>For build tigger concept with aws and jenkins are better parts of pipeline </p>
     </div>
   )
 }
