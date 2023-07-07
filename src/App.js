6d35d906d2a9cff948fb664ca1d9e944d97cbfe0
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-07 09:26:24 +0530

    this my chace

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <div >
@@ -12,7 +13,11 @@ const App = () => {
       <p>For build and deploy</p>
       <p>For build trigger </p>
       <p>For build tigger concept with aws and jenkins are better parts of pipeline </p>
+      <div>
+      <Footer />
+      </div>
     </div>
+
   )
 }
 
